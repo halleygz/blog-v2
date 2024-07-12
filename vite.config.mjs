c0c406c15a4118cbd7a9fdcce19ab16026e6e263
Halleluya Mulugeta <98106633+halleygz@users.noreply.github.com>
2024-07-12 18:13:41 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,8 @@ export default defineConfig({
     outDir: "build",
   },
   server: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
   },
   plugins: [react()],
   envPrefix: "VITE_"
