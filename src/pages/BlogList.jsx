b7ca2e26c1b0dc8e58444360d8c579f9e3e15e70
Emanah19 <zb69726@gmail.com>
2024-07-13 17:04:57 +0100

    Style change to the BlogList

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -45,46 +45,50 @@ const BlogList = ({ getMeOut }) => {
   };
 
   return (
-    <div className="w-full relative bg-whitesmoke overflow-y-auto flex flex-col items-start justify-start pt-4 pb-[373px] pr-[34px] pl-9 box-border gap-[35px] leading-[normal] tracking-[normal] text-left text-xl text-gray-200 font-lexend-deca mq675:gap-[17px]">
-      <Navbar loggedState={currentUser} fLetter={currentUser?.email[0]} logout={getMeOut} />
+    <div className="bg-[#e6b9a6]">
+      <div className="mx-auto max-w-4xl relative bg-whitesmoke overflow-y-auto flex flex-col items-start justify-start pt-4 pb-[373px] pr-[34px] pl-9 box-border gap-[35px] leading-[normal] tracking-[normal] text-left text-xl text-gray-200 font-lexend-deca mq675:gap-[17px]">
+        <Navbar loggedState={currentUser} fLetter={currentUser?.email[0]} logout={getMeOut} />
 
-      <section className="self-stretch flex flex-row items-start justify-center py-0 pr-0 pl-[34px] box-border max-w-full mt-10">
-        <div className="self-stretch flex flex-col items-start justify-start gap-[35px] max-w-full mq750:gap-[17px]">
-          <div className="flex flex-row items-start justify-start py-0 px-[9px]">
-            <div className="flex flex-col items-start justify-start gap-[4px]">
-              <div className="flex flex-row items-start justify-start py-0 pr-[22px] pl-[19px]">
-                <div className="h-1 w-5 relative bg-tan" />
+        <section className="self-stretch flex flex-row items-start justify-center py-0 pr-0 pl-[34px] box-border max-w-full mt-10">
+          <div className="self-stretch flex flex-col items-start justify-start gap-[35px] max-w-full mq750:gap-[17px]">
+            <div className="flex flex-row items-start justify-start py-0 px-[9px]">
+              <div className="flex flex-col items-start justify-start gap-[4px]">
+                <div className="flex flex-row items-start justify-start py-0 pr-[22px] pl-[19px]">
+                  <div className="h-1 w-5 relative bg-tan" />
+                </div>
+                <a className="[text-decoration:none] relative text-[inherit] inline-block min-w-[61px] mq450:text-base">
+                  Latest
+                </a>
               </div>
-              <a className="[text-decoration:none] relative text-[inherit] inline-block min-w-[61px] mq450:text-base">
-                Latest
-              </a>
             </div>
-          </div>
 
-          <div className="self-stretch flex flex-col items-start justify-start gap-[2px] max-w-full cursor-pointer">
-            <FeedDesktop contentData={{ ...contentData }} />
-            
-            
-            <div className="w-[500px] flex flex-row items-start justify-start py-0 px-2.5 box-border">
-              <TagBtns content="#Something" />
-              <TagBtns content="#and there" />
-              <TagBtns content="post details" />
+            <div className="self-stretch flex flex-col items-start justify-start gap-[2px] max-w-full cursor-pointer">
+              <FeedDesktop contentData={{ ...contentData }} />
+              <br/>
+              
+              <div className="w-[500px] flex flex-row items-start justify-start py-0 px-2.5 box-border">
+                <TagBtns content="#Something" />
+                <TagBtns content="#and there" />
+                <TagBtns content="post details" />
+              </div>
             </div>
-          </div>
-          {data.map((item) => (
-          <div key={item.id} className="self-stretch flex flex-col items-start justify-start gap-[2px] max-w-full cursor-pointer">
-            <FeedDesktop contentData={{ ...item }} />
-            <div className="w-[500px] flex flex-row items-start justify-start py-0 px-2.5 box-border">
-              {item.tags.map((tag)=>(
-              <TagBtns  content={tag} />
-              ))}
-              <TagBtns content="post details" />
+            {data.map((item) => (
+            <div key={item.id} className="self-stretch flex flex-col items-start justify-start gap-[2px] max-w-full cursor-pointer">
+              <FeedDesktop contentData={{ ...item }} />
+              <br/>
+              
+              <div className="w-[500px] flex flex-row items-start justify-start py-0 px-2.5 box-border">
+                {item.tags.map((tag)=>(
+                <TagBtns  content={tag} />
+                ))}
+                <TagBtns content="post details" />
+              </div>
             </div>
-          </div>
 
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
