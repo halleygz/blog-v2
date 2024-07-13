b0473ce3d1f1e7fb6b968f500ae0ae7a145a6acc
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-07-14 02:13:28 +0300

    optimized page, add tags functionality added

--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -4,23 +4,35 @@ import Buttons from "../components/Tools/Buttons";
 import FroalaEditor from "react-froala-wysiwyg";
 import "froala-editor/css/froala_style.min.css";
 import "froala-editor/css/froala_editor.pkgd.min.css";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useMemo } from "react";
 import MarkdownPreview from "@uiw/react-markdown-preview";
 import { blogCollection, userCollection } from "../firebase";
-import { collection, addDoc, onSnapshot, query, getDocs, where, getDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  getDocs,
+  where,
+  getDoc,
+  doc,
+} from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import debounce from "lodash/debounce";
 
 const AddBlog = () => {
   const [model, setModel] = useState("");
   const [title, setTitle] = useState("");
-  const [user, setUser] = useState({ username: "@halle" }); // replace with actual user state
+  const [user, setUser] = useState({ username: "@halle" });
   const [blog, setBlog] = useState([]);
+  const [tags, setTags] = useState("");
+  const [tagsArray, setTagsArray] = useState([]);
   const navigate = useNavigate();
   const { currentUser } = useAuth();
-  
+
   const getUserName = useCallback(async () => {
-    const q1 = query(userCollection, where('email', "==", currentUser.email));
+    const q1 = query(userCollection, where("email", "==", currentUser.email));
     const querySnapshot1 = await getDocs(q1);
     if (!querySnapshot1.empty) {
       const userDoc = querySnapshot1.docs[0];
@@ -29,6 +41,15 @@ const AddBlog = () => {
     }
   }, [currentUser.email]);
 
+  const onTagsChange = useMemo(
+    () => (event) => {
+      const value = event.target.value;
+      setTags(value);
+      const resultArray = value.replace(/\s+/g, "").split(",");
+      setTagsArray(resultArray);
+    },
+    []
+  );
   useEffect(() => {
     if (currentUser) {
       getUserName();
@@ -53,8 +74,8 @@ const AddBlog = () => {
       updatedAt: "",
       title: title,
       content: model,
-      snippetContent: model.slice(0, 300), // snippet of the first 300 characters
-      tags: ["tag1", "tag2", "tag3"],
+      snippetContent: model.slice(0, 300),
+      tags: tagsArray,
       views: 0,
       comments: [],
     };
@@ -66,12 +87,11 @@ const AddBlog = () => {
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-  }, [user.username, title, model, navigate]);
-  
+  }, [user.username, title, model, tagsArray, navigate]);
+
   return (
     <div className="w-full relative bg-whitesmoke overflow-hidden flex flex-row items-center justify-center gap-[71px] leading-[normal] tracking-[normal] text-center text-45xl text-darkslategray font-lexend-deca lg:flex-wrap lg:gap-[35px] mq750:gap-[18px]">
       {/* <SideJumb content="Try Buddy"/> */}
-      
       <div className="w-[670px] flex flex-col items-start justify-start pt-[46px] px-0 pb-0 box-border min-w-[670px] max-w-full text-left text-5xl lg:flex-1 mq450:pt-5 mq450:box-border mq750:min-w-full mq1050:pt-[30px] mq1050:box-border">
         <div className="self-stretch flex flex-col items-start justify-start gap-[13px] max-w-full">
           <div className="self-stretch flex flex-row items-start justify-start py-0 px-12 box-border max-w-full text-center text-29xl font-dm-serif-display mq750:pl-6 mq750:pr-6 mq750:box-border">
@@ -104,19 +124,27 @@ const AddBlog = () => {
             <FroalaEditor model={model} onModelChange={setModel} />
           </div>
           <div className="inline">
+            {tagsArray.map((atag) => (
+              <code>#{atag}</code>
+            ))}
+          </div>
+          <div className="inline">
             <label
               htmlFor="tags"
               className="mr-5 relative text-5xl leading-[24px] font-light font-lexend-deca text-darkslategray text-left inline-block mq450:text-lgi mq450:leading-[19px]  min-w-[117px]"
             >
-              <button className="h-12 w-[167.7px] text-[1.3rem] text-[#2f3645] font-normal font-lexend-deca bg-[#e6b9a6] ">Add tags</button>
+              <button className="h-12 w-[167.7px] text-[1.3rem] text-[#2f3645] font-normal font-lexend-deca bg-[#e6b9a6] ">
+                Add tags
+              </button>
             </label>
             <input
               className="[outline:none] bg-white self-stretch h-12 relative box-border  min-w-[480px] mt-1 "
               type="text"
               id="tags"
               name="tags"
-              // value={value}
-              // onChange={onChange}
+              placeholder="tag1, tag2, tag3"
+              value={tags}
+              onChange={onTagsChange}
             />
           </div>
           <div className="self-stretch flex flex-row items-start justify-center py-0 px-5">
